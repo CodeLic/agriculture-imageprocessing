ace551ebeb9dbc93d7a88ea7fe48bc31092b35d4
kazusa-hatakeyama <hatakeyama424kzs@gmail.com>
2022-04-05 11:53:41 +0900

    サーバーから snapshotしたARマーカを検出する

---
+++ DictAruco.py
@@ -0,0 +1,43 @@
+#ライブラリimport
+from urllib import response
+import cv2
+import requests
+
+# arucoのインスタンス
+aruco=cv2.aruco
+
+# 判定用
+DICT = False
+
+# 画像のURL
+URL = "http://192.168.2.164:8080/?action=snapshot"
+
+# 4ブロック×4ブロックを50個使用可能
+p_dict = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+
+# 撮影した画像の受け取り
+response = requests.get(URL)
+img = response.content
+
+
+# 取得確認表示
+if(img):
+    print("GET_PICS")
+else:
+    print("Undifind")
+
+# imgに撮影した画像を格納していきましょう
+# img = cv2.imread('/content/drive/MyDrive/IMG_0086.jpg')
+
+# False用テスト
+# img = cv2.imread('/content/drive/MyDrive/CVCameraCalibrateImages/ElemImage/SAMPLE_NEAR.jpg')
+
+# 検出
+corners, ids, rejectedImgPoints = aruco.detectMarkers(img, p_dict)
+
+if (rejectedImgPoints):
+    DICT = True
+    print(DICT)
+else:
+    DICT = False
+    print(DICT)
