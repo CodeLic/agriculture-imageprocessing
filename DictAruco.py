1a9588cc7e39d6e3d0e31d1db0df3f938e27a432
adachi-codelic <adachi-t@codelic.co>
2022-04-28 17:51:17 +0900

    ずれを検知する関数追加

--- DictAruco.py
+++ DictAruco.py
@@ -4,57 +4,60 @@ import cv2
 import requests
 import numpy as np
 
-# arucoのインスタンス
-ARUCO=cv2.aruco
+def getCorners():
+    # arucoのインスタンス
+    ARUCO=cv2.aruco
 
-# 判定用
-DICT = False
+    # 判定用
+    DICT = False
 
-# 画像のURL
-URL = "http://192.168.2.164:8080/?action=snapshot"
+    # 画像のURL
+    URL = "http://192.168.2.164:8080/?action=snapshot"
 
-# 4ブロック×4ブロックを50個使用可能
-p_dict = ARUCO.getPredefinedDictionary(ARUCO.DICT_4X4_50)
+    # 4ブロック×4ブロックを50個使用可能
+    p_dict = ARUCO.getPredefinedDictionary(ARUCO.DICT_4X4_50)
 
-# 撮影した画像の受け取り
-response = requests.get(URL)
-img = response.content
+    # 撮影した画像の受け取り
+    response = requests.get(URL)
+    img = response.content
 
-# 取得確認表示
-image = None
-if(img):
-    print("GET_PICS")
-    image = np.asarray(bytearray(img), dtype='uint8')
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    cv2.imshow('test', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # 取得確認表示
+    image = None
+    if(img):
+        print("GET_PICS")
+        image = np.asarray(bytearray(img), dtype='uint8')
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        cv2.imshow('test', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-else:
-    print("Undifind")
+    else:
+        print("Undefind")
+        return
 
-#with open("GetPic.jpg", "wb") as snapshot:
-#    snapshot.write(img)
+    #with open("GetPic.jpg", "wb") as snapshot:
+    #    snapshot.write(img)
 
-# imgに撮影した画像を格納していきましょう
-# image = cv2.imread('/home/pi/agriculture-imageprocessing/GetPic.jpg')
+    # imgに撮影した画像を格納していきましょう
+    # image = cv2.imread('/home/pi/agriculture-imageprocessing/GetPic.jpg')
 
-# arucoマーカー検出
-corners, ids, rejectedImgPoints = ARUCO.detectMarkers(image, p_dict)
+    # arucoマーカー検出
+    corners, ids, rejectedImgPoints = ARUCO.detectMarkers(image, p_dict)
 
-#if (rejectedImgPoints):
-#    print(ids)
-#    print(corners)
-#    print(rejectedImgPoints)
-#    DICT = True
-#    print(DICT)
-#else:
-#    DICT = False
-#    print(DICT)
-if ids != None:
-    print('succeed on detecting')
-    print(ids)
-    print(corners)
-
-else:
-    print('fail')
+    #if (rejectedImgPoints):
+    #    print(ids)
+    #    print(corners)
+    #    print(rejectedImgPoints)
+    #    DICT = True
+    #    print(DICT)
+    #else:
+    #    DICT = False
+    #    print(DICT)
+    if ids != None:
+        print('succeed on detecting')
+        print(ids)
+        print(corners)
+        return corners[0]
+    else:
+        print('fail')
+        return
---
+++ detect_diff.py
@@ -0,0 +1,30 @@
+from DictAruco import getCorners
+
+def detect_diff():
+  height = 720 # px
+  center = height / 2
+  threshold = 10 # px
+  corners = getCorners()
+  if corners != None:
+    # マーカーの取得成功
+    left_bottom = corners[0]
+    left_top = corners[1]
+    right_top = corners[2]
+    right_bottom = corners[3]
+
+    # マーカーは水平と仮定する
+    # 左側だけで検査すればよい
+    # 右側でも検査して結果がある程度以上ずれればマーカーが傾いていることも検知できる
+      
+    # 中点取得
+    marker_center = left_top[1] + left_bottom[1] / 2
+    diff = center - marker_center
+    if abs(diff) > threshold:
+      print('ずれています')
+    else:
+      print('ずれていません')
+    print(diff)
+    return marker_center - center
+
+if __name__ == '__main__':
+  detect_diff()
